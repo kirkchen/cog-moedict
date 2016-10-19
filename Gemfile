d08c02a51c5a5bbe01190cce6a800d1e3e9fd4c1
Kirk Chen <rwk0119@yahoo.com.tw>
2016-10-20 00:59:58 +0800

    Initial project

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'cog-rb', github: "operable/cog-rb"
---
+++ lib/cog_cmd/moedict/image.rb
@@ -0,0 +1,17 @@
+﻿require 'cog/command'
+require 'net/http'
+require 'json'
+require 'cgi'
+
+class CogCmd::Moedict::Image < Cog::Command
+  API_BASE = "https://www.moedict.tw"
+
+  def run_command
+    condition = CGI.escape(request.args[0])
+
+		response.template = "image"
+		response.content = {
+			image: "#{API_BASE}/#{condition}.png",
+    }
+  end
+end
